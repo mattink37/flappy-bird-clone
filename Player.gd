262934c62953f75fbb26856dea8ba61a31680810
Matt Inkeles <mattinkeles37@gmail.com>
2024-04-20 17:45:18 -0400

    added player gravity + jump

--- Player.gd
+++ Player.gd
@@ -1,7 +1,15 @@
 extends CharacterBody2D
 
-func _ready():
-	pass
-	
-func _process(delta):
-	pass
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept", true):
+		velocity.y = JUMP_VELOCITY
+	move_and_slide()
