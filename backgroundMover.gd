ee95e497ec1c709d1348e5989c470779795e08dc
Matt Inkeles <mattinkeles37@gmail.com>
2024-04-21 00:06:32 -0400

    added moving pipes

--- backgroundMover.gd
+++ backgroundMover.gd
@@ -3,10 +3,14 @@ extends Node2D
 var bg1
 var bg2
 var bgWidth
-var bgSpeed = 50  # Speed of the background movement in pixels per second.
-var backgroundScene = preload("res://background_tile.tscn")
+var bgSpeed = 20  # Speed of the background movement in pixels per second.
+var backgroundScene: PackedScene = preload("res://background_tile.tscn")
+var pipeScene: PackedScene = preload("res://pipe.tscn")
+@onready var pipeTimer: Timer
+
 
 func _ready():
+	pipeHandler()
 	backdropLoad()
 
 func _process(delta):
@@ -28,3 +32,15 @@ func backdropHandler(delta):
 		bg1.position.x = bgWidth - 1
 	if bg2.position.x < -bgWidth:
 		bg2.position.x = bgWidth - 1
+		
+func pipeHandler():
+	var pipeTimer: Timer = Timer.new()
+	add_child(pipeTimer)
+	pipeTimer.wait_time = 1.75
+	pipeTimer.connect("timeout", spawnPipe)
+	pipeTimer.start()
+		
+func spawnPipe():
+	var newPipe := pipeScene.instantiate()
+	get_node("Pipes").add_child(newPipe)
+	newPipe.position.x = 550
